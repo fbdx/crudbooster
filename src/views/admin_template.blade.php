ca1576a811457da3c1231494b89a8b3663007e9f
Adrian Lim <adrian.lbs@gmail.com>
2018-10-18 19:27:58 +0800

    Updated crudbooster admin to give meta tag for vuejs router to pick up path

--- src/views/admin_template.blade.php
+++ src/views/admin_template.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>{{ ($page_title)?Session::get('appname').': '.strip_tags($page_title):"Admin Area" }}</title>
+    <meta name="api-base-url" content="{{ url('/') }}" />
     <meta name="csrf-token" content="{{ csrf_token() }}"/>
     <meta name='generator' content='CRUDBooster 5.4.6'/>
     <meta name='robots' content='noindex,nofollow'/>
