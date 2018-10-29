f8aa22b8ebf573eb79cb946f51a61c214b75bcbc
Adrian Lim <adrian.lbs@gmail.com>
2018-10-29 18:49:22 +0800

    Updated bootstrap toggle and also added in ddslick

--- src/views/admin_template_plugins.blade.php
+++ src/views/admin_template_plugins.blade.php
@@ -26,9 +26,17 @@
 <link rel="stylesheet" href="{{ asset ('vendor/crudbooster/assets/adminlte/plugins/timepicker/bootstrap-timepicker.min.css') }}">
 <script src="{{ asset ('vendor/crudbooster/assets/adminlte/plugins/timepicker/bootstrap-timepicker.min.js') }}"></script>
 
+
+<!-- ddSlick jquery -->
+<script src="{{ asset('vendor/crudbooster/assets/js/jquery.ddslick.min.js') }}"></script>
+
+
+
 <link rel='stylesheet' href='{{ asset("vendor/crudbooster/assets/lightbox/dist/css/lightbox.min.css") }}'/>
 <script src="{{ asset('vendor/crudbooster/assets/lightbox/dist/js/lightbox.min.js') }}"></script>
 
+
+<link href="https://gitcdn.github.io/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css" rel="stylesheet">
 <script src="https://gitcdn.github.io/bootstrap-toggle/2.2.2/js/bootstrap-toggle.min.js"></script>
 
 <!--SWEET ALERT-->
