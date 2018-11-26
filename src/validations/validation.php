d209d6240ee203987edc7932ada86bffd4c81a75
Adrian Lim <adrian.limbs@cimb.com>
2018-11-26 09:37:13 +0800

    Removed validation for debugging

--- src/validations/validation.php
+++ src/validations/validation.php
@@ -6,8 +6,8 @@
     return preg_match('/^[\pL\s]+$/u', $value); 
 },'The :attribute should be letters only');
 
-\Validator::extend('passwordregex', function ($attribute, $value) {
+/*\Validator::extend('passwordregex', function ($attribute, $value) {
     // This will only accept alpha and spaces. 
     // If you want to accept hyphens use: /^[\pL\s-]+$/u.
     return preg_match('^.*(?=.{3,})(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[\d\x])(?=.*[!$#%]).*$', $value); 
-},'The :attribute should have an Uppercase, lowercase, number and a symbol');
+},'The :attribute should have an Uppercase, lowercase, number and a symbol');*/
