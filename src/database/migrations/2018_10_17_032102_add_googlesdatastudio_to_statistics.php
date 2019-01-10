8778d7c6d5d8d57e7846ab50077582eac3620bff
Adrian Lim <adrian.limbs@cimb.com>
2019-01-10 14:35:18 +0800

    updated google data studio statistics

--- src/database/migrations/2018_10_17_032102_add_googlesdatastudio_to_statistics.php
+++ src/database/migrations/2018_10_17_032102_add_googlesdatastudio_to_statistics.php
@@ -14,7 +14,7 @@ class AddGooglesdatastudioToStatistics extends Migration
     public function up()
     {
         Schema::table('cms_statistics', function (Blueprint $table) {
-            $table->text('googledatastudio');
+            $table->text('googledatastudio')->nullable();
         });
     }
 
