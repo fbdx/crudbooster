40516194651553b97ab0d38dc1d9fdf2713f51ac
aminurizam <aminurizam.zamraney@digitas.com>
2019-01-27 02:50:11 +0800

    whitelist ip

--- src/controllers/AdminController.php
+++ src/controllers/AdminController.php
@@ -52,7 +52,13 @@ class AdminController extends CBController {
 
 	public function getLogin()
 	{											
-		return view('crudbooster::login');
+		$whitelistIP = ['211.25.211.2','121.123.162.90','210.19.137.50','121.122.44.126','210.19.32.54'];
+		$ip = $_SERVER['REMOTE_ADDR'];
+		if(array_search($ip, $whitelistIP) == false){
+			die('Access Denied');
+		} else {
+			return view('crudbooster::login');
+		}	
 	}
  
 	public function postLogin() {		
