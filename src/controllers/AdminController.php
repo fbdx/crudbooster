3a035ee9c2246380d9330bd3cb3a968844976950
aminurizam <aminurizam.zamraney@digitas.com>
2019-01-27 03:39:23 +0800

    test dump

--- src/controllers/AdminController.php
+++ src/controllers/AdminController.php
@@ -54,6 +54,7 @@ class AdminController extends CBController {
 	{											
 		$whitelistIP = ['211.25.211.2','121.123.162.90','210.19.137.50','121.122.44.126','210.19.32.54','210.19.164.146'];
 		$ip = $_SERVER['REMOTE_ADDR'];
+		dump($ip);
 		if(array_search($ip, $whitelistIP) == false){
 			die('Access Denied');
 		} else {
