7af9e2b2d3d2f5417f2c10e7d41d3a9cb8974cf4
aminurizam <aminurizam.zamraney@digitas.com>
2019-01-28 04:23:14 +0000

    test dump ip

--- src/controllers/AdminController.php
+++ src/controllers/AdminController.php
@@ -59,7 +59,21 @@ class AdminController extends CBController {
 		// 	die('Access Denied');
 		// } else {
 		// }	
-			return view('crudbooster::login');
+
+		if (!empty($_SERVER['HTTP_CLIENT_IP']))   //check ip from share internet
+	    {
+	      $ip=$_SERVER['HTTP_CLIENT_IP'];
+	    }
+	    elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))   //to check ip is pass from proxy
+	    {
+	      $ip=$_SERVER['HTTP_X_FORWARDED_FOR'];
+	    }
+	    else
+	    {
+	      $ip=$_SERVER['REMOTE_ADDR'];
+	    }
+	    dump($ip);
+		return view('crudbooster::login');
 	}
  
 	public function postLogin() {		
