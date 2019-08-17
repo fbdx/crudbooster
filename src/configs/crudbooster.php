64c1c32d018ed05aeea8fedb5a42a0a4592a585d
Adrian Lim <adrian.lim@digitas.com>
2019-08-17 16:44:14 +0800

    Added flagg for IP_LOCKED, to be used for flag IP LOCKED

--- src/configs/crudbooster.php
+++ src/configs/crudbooster.php
@@ -1,15 +1,15 @@
 <?php
 
 return [
-	
+
 	'ADMIN_PATH'                =>'admin',
 
 	'USER_TABLE'				=>'cms_users',
-	
+
 	'IMAGE_FIELDS_CANDIDATE'    =>'image,picture,photo,photos,foto,gambar,thumbnail',
-	
+
 	'PASSWORD_FIELDS_CANDIDATE' =>'password,pass,pwd,passwrd,sandi,pin',
-	
+
 	'DATE_FIELDS_CANDIDATE'     =>'date,tanggal,tgl,created_at,updated_at,deleted_at',
 
 	'EMAIL_FIELDS_CANDIDATE'	=>'email,mail,email_address',
@@ -19,7 +19,7 @@ return [
 	'NAME_FIELDS_CANDIDATE'		=>'name,nama,person_name,person,fullname,full_name,nickname,nick,nick_name,title,judul,content',
 
 	'URL_FIELDS_CANDIDATE'		=>'url,link',
-	
+
 	'UPLOAD_TYPES'              =>'jpg,png,jpeg,gif,bmp,pdf,xls,xlsx,doc,docx,txt,zip,rar,7z',
 
 	'DEFAULT_THUMBNAIL_WIDTH' 	=>300,
@@ -66,8 +66,10 @@ return [
 
 	'ADMIN_LAYOUT'	=> '',
 
-	/* 
-	* NOTE : 
+	'IP_LOCKED' => env('IP_LOCKED',true),
+
+	/*
+	* NOTE :
 	* As a default, CRUDBooster making a cache for the configuration with command `php artisan config:cache`. It means any changes that you've made,
 	* would not make an effect, until you run `php artisan config:cache` again.
 	*
