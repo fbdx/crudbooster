565405943a23797baa29b244613903aba89dd914
Lee Chun Gui <brave1028_@hotmail.com>
2020-03-18 10:37:13 +0000

    update routes to enable google 2fa

--- src/routes.php
+++ src/routes.php
@@ -44,7 +44,8 @@ Route::group(['middleware'=>['web'],'prefix'=>config('crudbooster.ADMIN_PATH'),'
 	Route::post('login', ['uses'=>'AdminController@postLogin','as'=>'postLogin']);
 	Route::get('change-password', ['uses' => 'AdminController@getChangePassword', 'as' => 'getChangePassword']);
 	Route::post('change-password', ['uses' => 'AdminController@postChangePassword', 'as' => 'postChangePassword']);
-
+	Route::get('/2fa/validate', ['uses' => 'AdminController@getValidateToken', 'as' => 'getValidateToken']);
+	Route::post('/2fa/validate', ['uses' => 'AdminController@postValidateToken', 'as' => 'postValidateToken']);
 });
 
 // ROUTER FOR OWN CONTROLLER FROM CB
