3fc76ac463af280bea3d2aed5a0d0c938fb81a26
Lee Chun Gui <chungui.lee@digitas.com>
2020-05-04 07:45:11 +0100

    reset proengsoft

--- src/views/admin_template_plugins.blade.php
+++ src/views/admin_template_plugins.blade.php
@@ -12,10 +12,10 @@
 	
 	<!-- REQUIRED JS SCRIPTS -->
 
-	<!-- jQuery 2.1.3 -->
+	<!-- jQuery 3.5.0 -->
 	<script src="{{ asset ('vendor/crudbooster/assets/adminlte/plugins/jQuery/jQuery-3.5.0.min.js') }}"></script>
 	
-	<!-- Bootstrap 3.3.2 JS -->
+	<!-- Bootstrap 4.4.1 JS -->
 	<script src="{{ asset ('vendor/crudbooster/assets/adminlte/bootstrap/js/bootstrap.min.js') }}" type="text/javascript"></script>
 	<!-- AdminLTE App -->
 	<script src="{{ asset ('vendor/crudbooster/assets/adminlte/dist/js/app.js') }}" type="text/javascript"></script>
